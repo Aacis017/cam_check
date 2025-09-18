81dfeddb8055d05e16a7aced86776f8441e52feb
RedDevil <121215956+Aacis017@users.noreply.github.com>
2025-09-18 16:29:10 +0900

    check

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, Response, render_template
+from picamera2 import Picamera2
+import cv2
+
+app = Flask(__name__)
+
+
+# Initialize PiCamera2
+picam2 = Picamera2()
+config = picam2.create_preview_configuration(main={"size": (640, 480)})
+picam2.configure(config)
+picam2.start()
+
+def generate_frames():
+    while True:
+        frame = picam2.capture_array()
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
