957c09d788befb1ae87db5e779df330f5549bdc9
RedDevil <121215956+Aacis017@users.noreply.github.com>
2025-09-19 10:46:54 +0900

    newwithoutopencv

--- app.py
+++ app.py
@@ -1,44 +1,39 @@
 from flask import Flask, Response, render_template_string
 from picamera2 import Picamera2
-import cv2
+import io
 
 app = Flask(__name__)
 
-# Initialize camera
+# Init camera
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(
-    main={"size": (640, 480)},
-    controls={"FrameRate": 15}
-))
+config = picam2.create_video_configuration(main={"size": (640, 480)})
+picam2.configure(config)
 picam2.start()
 
-# HTML Template
-html_template = """
+# HTML Page
+html = """
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Pi Camera Stream</title>
-</head>
+<head><title>Pi Camera Stream</title></head>
 <body>
-    <h1>📷 Raspberry Pi Zero 2W Camera</h1>
-    <img src="{{ url_for('video_feed') }}" width="640" height="480">
+  <h1>📷 Raspberry Pi Zero 2W Camera Stream</h1>
+  <img src="{{ url_for('video_feed') }}" width="640" height="480">
 </body>
 </html>
 """
 
 def generate_frames():
     while True:
-        frame = picam2.capture_array()
-        ret, buffer = cv2.imencode('.jpg', frame)
-        if not ret:
-            continue
-        frame_bytes = buffer.tobytes()
+        buf = io.BytesIO()
+        # Capture JPEG directly into buffer
+        picam2.capture_file(buf, format='jpeg')
+        frame = buf.getvalue()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-    return render_template_string(html_template)
+    return render_template_string(html)
 
 @app.route('/video_feed')
 def video_feed():
